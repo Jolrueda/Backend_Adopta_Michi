42f745594e85cfcb30b87481ba4362d8f8722858
Arley56 <dbolivard@unal.edu.co>
2025-07-12 21:30:15 -0500

    Edicion a donationController

--- src/routes/donationRoutes.ts
+++ src/routes/donationRoutes.ts
@@ -5,7 +5,8 @@ import protect from '../middlewares/authMiddleware';
 const router = Router();
 
 router.post('/', createDonation);
-router.get('/', protect, listDonations);
-router.get('/:id', protect, getDonationById);
+router.get('/', /* protect, */ listDonations);
+router.get('/:id', /* protect, */ getDonationById);
+
 
 export default router; 
